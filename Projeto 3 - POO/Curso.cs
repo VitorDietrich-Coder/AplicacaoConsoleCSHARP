50331c98144feff3969c86db8721a819d35f942e
Vitor Dietrich <85202068+VitorDietrich-Coder@users.noreply.github.com>
2021-12-13 22:13:09 -0300

    Add files via upload

---
+++ Projeto 3 - POO/Curso.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_3___POO
+{
+    [System.Serializable]
+    class Curso : Produto, IEstoque
+    {
+        public string autor;
+        private int vagas;
+        public Curso(string nome, float preço, string autor) 
+        {
+            this.nome = nome;
+            this.preço = preço;
+            this.autor = autor;
+        }
+
+        public void adicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar vagas no curso : {nome}");
+            Console.WriteLine("Digite a quantidade que vagas que quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vagas += entrada;
+            Console.WriteLine("Entrada registrada");
+            Console.ReadLine();
+
+        }
+        public void adicionarSaida()
+        {
+            Console.WriteLine($"Dar saída nas vagas do curso: {nome}");
+            Console.WriteLine("Digite a quantidade  de vagas que você quer dar saída: ");
+            int saida = int.Parse(Console.ReadLine());
+            vagas -= saida;
+            Console.WriteLine("Saída registrada");
+            Console.ReadLine();
+        }
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preço}");
+            Console.WriteLine($"Vagas restantes: {vagas}");
+           
+          
+        }
+    }
+}
