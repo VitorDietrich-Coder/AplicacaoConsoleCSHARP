955d71f096b5c5bcee8b3de429450e21acaf7d12
Vitor Dietrich <85202068+VitorDietrich-Coder@users.noreply.github.com>
2021-12-13 22:12:20 -0300

    Add files via upload

---
+++ Projeto 3 - Orientado a Obj/Projeto 3 - POO/Ebook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_3___POO
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+        public Ebook(string nome, float preço, string autor)
+        {
+            this.nome = nome;
+            this.preço = preço;
+            this.autor = autor;
+        }
+
+        public void adicionarEntrada()
+        {
+            Console.WriteLine("Não é possível dar entrada no estoque do ebook, pois é um produto virtual");
+            Console.ReadLine();
+        }
+        public void adicionarSaida()
+        {
+            Console.WriteLine("Não é possível dar saída no estoque do ebook, pois é um produto virtual");
+            Console.ReadLine();
+
+        }
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preço}");
+            Console.WriteLine($"Vendas restantes: {vendas}");
+        }
+    }
+}
