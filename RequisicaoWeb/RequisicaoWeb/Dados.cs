b4727120d3039b6491d0086e8d5dfed629453a98
Vitor Dietrich <85202068+VitorDietrich-Coder@users.noreply.github.com>
2021-12-14 19:50:47 -0300

    Add files via upload

---
+++ RequisicaoWeb/RequisicaoWeb/Dados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoWeb
+{
+    internal class Dados
+    {
+        public int Userid;
+        public int id;
+        public string title;
+        public bool completed;
+
+
+        public void Exibir() 
+        {
+            Console.WriteLine($"User: {Userid}");
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"titúlo: {title}");
+            Console.WriteLine($"status: {completed}");
+
+        }
+    }
+}
