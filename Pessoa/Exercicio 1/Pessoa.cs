33777d0397669f7b9a240a73e5ec32a957887523
Vitor Dietrich <85202068+VitorDietrich-Coder@users.noreply.github.com>
2022-01-25 00:05:55 -0300

    Add files via upload

---
+++ Pessoa/Exercicio 1/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    public class Pessoa
+    {
+        public  string nome;
+        public int idade;
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+
+        }
+
+       
+    }
+}
---
+++ Pessoa/Exercicio 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            maisVelho();
+        }
+
+        public static  void maisVelho()
+        {
+           
+            Console.WriteLine("nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("idade:");
+            int idade = int.Parse( Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(nome, idade);
+
+            Console.WriteLine("nome:");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("idade:");
+            int idade2 = int.Parse(Console.ReadLine());
+
+            Pessoa pessoa2 = new Pessoa(nome, idade);
+
+            if (idade > idade2)
+            {
+                Console.WriteLine("Pessoa mais velha: " + nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + nome2);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
